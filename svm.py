84b3468591dc05a6bea1c712db78184f6c3ea1e5
christiankiesewetter <corkscrew.ki@gmail.com>
2021-12-28 14:35:37 +0100

    running version rbf

--- svm.py
+++ svm.py
@@ -4,21 +4,28 @@ from tensorflow.keras.models import Model
 class SVMModel(Model):
 
     def rbf_kernel(self, X):
-        y = (X - self.means) / (2 * self.sigma**2)
+        X = tf.expand_dims(X, axis=-1)
+        y = tf.exp((X - self.landmarks)**2 / (2 * self.sigma**2))
         return y
 
 
     def linear_kernel(self, X):
-        y_hat = self.slope * X + self.intercept
+        y_hat = X * self.slope + self.intercept
         return y_hat
 
 
     def init_rbf_kernel(self):
-        init_mu = tf.random.uniform(shape = (1, self.dimensions), minval = 0., maxval = 1, dtype=tf.float32)
-        self.means = tf.Variable(init_mu, trainable = True, name = 'rbf_mu')
+        init_landmarks = tf.random.uniform(shape = (1, 1, self.n_landmarks), minval = -1., maxval = 1., dtype=tf.float32)
+        self.landmarks = tf.Variable(init_landmarks, trainable = True, name = 'rbf_mu')
 
-        init_sigma = tf.random.uniform(shape = (1, self.dimensions), minval = 0., maxval = 0.5, dtype=tf.float32)
+        init_sigma = tf.random.uniform(shape = (1, 1, self.n_landmarks), minval = 0.3, maxval = 3, dtype=tf.float32)
         self.sigma = tf.Variable(init_sigma, trainable = True, name = 'rbf_sigma')
+
+        init_intercept = tf.random.normal(shape = (1, 1, self.n_landmarks), mean=0.1, stddev=0.5, dtype=tf.float32)
+        self.intercept = tf.Variable(init_intercept, trainable = True, name = 'intercept')
+
+        init_slope = tf.random.normal(shape = (1, 1, self.n_landmarks), mean=-0.1, stddev=0.5, dtype=tf.float32)
+        self.slope = tf.Variable(init_slope, trainable = True, name = 'slope')
 
 
     def init_linear_kernel(self):
@@ -34,29 +41,32 @@ class SVMModel(Model):
 
 
     def h(self, X):
-        if self.use_rbf:
+        if self.n_landmarks:
             X = self.rbf_kernel(X)
-        y_hat = self.linear_kernel(X)
+            X = self.linear_kernel(X)
+            y_hat = tf.reshape(X, (-1, self.dimensions * self.n_landmarks))
+        else:
+            y_hat = self.linear_kernel(X)
         y_hat = tf.expand_dims(tf.reduce_sum(y_hat, axis = 1), axis=-1)
-        #y_hat = tf.maximum(y_hat, 0)
         return y_hat
 
 
     def svm_loss(self, y, y_hat):
         loss = self.C * (
-                (0.5 * (1 + y) * tf.reduce_sum(self.hinge(-y_hat)))
-                + (0.5 * (1. - y) * tf.reduce_sum(self.hinge(y_hat)))) \
-                + 0.5 * (tf.reduce_sum(self.slope**2) + tf.reduce_sum(self.intercept**2))
+                (y * tf.reduce_sum(self.hinge(-y_hat)))
+                + ((1. - y) * tf.reduce_sum(self.hinge(y_hat)))) \
+                + 0.5 * (tf.reduce_sum(self.slope**2))
         return loss
 
 
-    def __init__(self, dimensions, C=1, rbf = False, **kwargs):
+    def __init__(self, dimensions, C=1, n_landmarks = None):
         super(SVMModel,self).__init__()
-        self.use_rbf = rbf
+        self.n_landmarks = n_landmarks
         self.dimensions = dimensions
-        if self.use_rbf:
+        if not self.n_landmarks is None:
             self.init_rbf_kernel()
-        self.init_linear_kernel()
+        else:
+            self.init_linear_kernel()
         self.C = C
 
 
@@ -73,7 +83,19 @@ class SVMModel(Model):
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         self.compiled_metrics.update_state(y, y_hat)
-        accuracy = 1. - tf.reduce_mean(tf.abs(y - tf.where(y_hat > 0, 1., -1.)))
+        accuracy = 1. - tf.reduce_mean(tf.abs(y - tf.where(y_hat > 0.5, 1., 0.)))
+        res = {
+            'acuracy': accuracy,
+            'loss':loss
+        }
+        return res
+
+    @tf.function
+    def test_step(self, data):
+        X, y = data
+        y_hat = self.call(X)
+        loss = self.svm_loss(y, y_hat)
+        accuracy = 1. - tf.reduce_mean(tf.abs(y - tf.where(y_hat > 0.5, 1., 0.)))
         res = {
             'acuracy': accuracy,
             'loss':loss
--- train.py
+++ train.py
@@ -7,8 +7,8 @@ def get_data(c=1):
     (X_train, y_train), (X_val , y_val) = load_data()
     y_train = np.expand_dims(y_train, axis=-1)
     y_val = np.expand_dims(y_val, axis=-1)
-    y_train = np.where(y_train == c, 1., -1.).astype(np.float32)
-    y_val = np.where(y_val == c, 1., -1.).astype(np.float32)
+    y_train = np.where(y_train == c, 1., 0.).astype(np.float32)
+    y_val = np.where(y_val == c, 1., 0.).astype(np.float32)
     X_train = X_train.reshape(X_train.shape[0], -1).astype(np.float32) / 127 - 1.
     X_val = X_val.reshape(X_val.shape[0], -1).astype(np.float32) / 127 - 1.
     return X_train, y_train, X_val, y_val
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     X_train, y_train, X_val, y_val = get_data()
     svm = SVMModel(
         dimensions = X_train.shape[1],
-        rbf = False,
-        C = 90)
+        n_landmarks = 10,
+        C = 1)
     svm.compile(optimizer=tf.keras.optimizers.Adam(lr=0.5e-03))
-    svm.fit(X_train, y_train, epochs = 100)
-    example = np.where(y_train == 1.)[0][1]
+    svm.fit(X_train, y_train, validation_data=(X_val, y_val), epochs = 5)
+    example = np.where(y_train == 1.)[0][20]
     print(svm(X_train[example,:]).numpy(), y_train[example,:])
